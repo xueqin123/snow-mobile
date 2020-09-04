4aa642c5def26360e0d334a93d27b1b4c8ce9dd3
qinxue <qinxue@didichuxing.com>
2020-09-04 16:57:42 +0800

    图片压缩

--- snow-client/lib/upload/uploader.dart
+++ snow-client/lib/upload/uploader.dart
@@ -1,9 +1,12 @@
 import 'dart:io';
 
+import 'package:flutter_image_compress/flutter_image_compress.dart';
 import 'package:snowclient/upload/tencent_cos.dart';
+import 'package:path_provider/path_provider.dart';
 
 class UpLoader {
   static const String _FILE_HOST = "https://bucket1-1257086843.cos.ap-beijing.myqcloud.com";
+  static const _UPLOAD_MAX_SIZE = 100;
 
   UpLoader._();
 
@@ -21,11 +24,31 @@ class UpLoader {
     _currentUid = uid;
   }
 
-  Future<String> uploadImage(String secretId, String secretKey, String token, String filePath) async {
+  Future<String> uploadImage(String secretId, String secretKey, String token, String originPath) async {
+    String compressedPath = await _compress(originPath);
     Cos cos = Cos(secretId, secretKey, _FILE_HOST, token);
-    String fileName = filePath.substring(filePath.lastIndexOf("/") + 1, filePath.length);
-    String upLoadName = _currentUid + fileName;
-    String imgUrl = await cos.upload('/image/$upLoadName', File(filePath).readAsBytesSync());
+    String suffix = compressedPath.substring(compressedPath.lastIndexOf("."), compressedPath.length);
+    String upLoadName = "${_currentUid}_${DateTime.now().millisecondsSinceEpoch}$suffix";
+    String imgUrl = await cos.upload('/image/$upLoadName', File(compressedPath).readAsBytesSync());
     return imgUrl;
+  }
+
+  Future<String> _compress(String originPath) async {
+    int originLength = File(originPath).lengthSync();
+    print("before compress file size: $originLength");
+    if (originLength < UpLoader._UPLOAD_MAX_SIZE) {
+      return originPath;
+    }
+    List<Directory> dirList = await getExternalCacheDirectories();
+    print("dirList length: ${dirList.length}");
+    for (Directory dir in dirList) {
+      print("dir path: ${dir.path}");
+    }
+    String cachePath = dirList[0].path;
+    String filename = originPath.substring(originPath.lastIndexOf("/") + 1);
+    String targetPath = "${cachePath}compressed_$filename";
+    File compressedFile = await FlutterImageCompress.compressAndGetFile(originPath, targetPath, quality: 50);
+    print("after compress file size: ${compressedFile.lengthSync()}");
+    return compressedFile.absolute.path;
   }
 }
