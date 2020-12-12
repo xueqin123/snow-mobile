84f40b0f0ae928d78e284643f1e430e942bdf5a2
qinxue <qinxue@didichuxing.com>
2020-12-12 22:40:29 +0800

    可以使用拍照功能发送图片

--- snow-client/lib/pages/message/widget/plugin/imp/camera_plugin.dart
+++ snow-client/lib/pages/message/widget/plugin/imp/camera_plugin.dart
@@ -2,8 +2,11 @@ import 'package:flutter/material.dart';
 import 'package:image_picker/image_picker.dart';
 import 'package:imlib/proto/message.pbenum.dart';
 import 'package:imlib/utils/s_log.dart';
+import 'package:provider/provider.dart';
 import 'package:snowclient/generated/l10n.dart';
 import 'package:snowclient/pages/message/widget/plugin/plugin.dart';
+
+import '../../../message_view_model.dart';
 
 class CameraPlugin extends Plugin{
   ImagePicker imagePicker;
@@ -25,8 +28,13 @@ class CameraPlugin extends Plugin{
 
   @override
   onClick(BuildContext context,String conversationId, ConversationType conversationType) async{
+    MessageViewModel viewModel = Provider.of<MessageViewModel>(context);
     PickedFile pickedFile = await imagePicker.getImage(source: ImageSource.camera);
     SLog.i("camera file: $pickedFile path:${pickedFile.path}");
+    String localPath = pickedFile.path;
+    if (localPath != null && localPath.isNotEmpty) {
+      viewModel.sendImageMessage(localPath);
+    }
   }
 
   CameraPlugin(){
